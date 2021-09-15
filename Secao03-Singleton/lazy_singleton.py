23a840564e2aaa4bb55a7bfbc9d742e3566429b5
CleberNandi <cleber.nandi@gmail.com>
2021-09-15 15:41:14 -0300

    Aprendendo padrão de projeto lazy singleton

---
+++ Secao03-Singleton/lazy_singleton.py
@@ -0,0 +1,20 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print(f"O Metodo __init__ foi chamado")
+        else:
+            print(f"A instancia já foi criada: {self.get_instance()}")
+    
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+s1 = Singleton()
+print(f"Objeto criado agora: {Singleton.get_instance()}")
+
+# s2 = Singleton()
+# print(f"Instancia 2: {id(s2)}")
